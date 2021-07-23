80b0edd5a34dc257b68a64efad4d70ba0c09bf1d
JUAN.TORRESVEGA <juan.torresvega@ucr.ac.cr>
2021-07-22 22:26:10 -0600

    session: cambio de nombre en game_board

--- src/js/client/main.js
+++ src/js/client/main.js
@@ -4,21 +4,31 @@ function main() {
     const game = new Game();
     //game.setup
 
+    //Se asigna para un jugador la selección de nombre y avatar
 
-    const nickname = window.localStorage.getItem('selectionofAvatars');
+    const nickname = window.localStorage.getItem('jugadorId');
+    const avatar = window.localStorage.getItem('selectionofAvatars');
 
-    const avatarJugador1 = document.getElementById('player1Avatar');
+    const player1Name = document.getElementById('player1Name');//Name of player
+    const avatarJugador1 = document.getElementById('player1Avatar');//Avatar at the side of the name of the player
+    const jugador1 = document.getElementById('Player1');//Token of player
+
+    player1Name.innerHTML=nickname;
     
-    if(nickname == "buffalo"){
+    //Se verifica y asigna el avatar seleccionado  *tal vez sirva más guardar y llamar las imagenes desde localStorage
+    if(avatar == "buffalo"){
         avatarJugador1.src= "img/048-buffalo.svg" ;
+        jugador1.src= "img/048-buffalo.svg" ;
     }
 
-    if(nickname == "tigre"){
+    if(avatar == "tigre"){
         avatarJugador1.src= "img/050-tiger-1.svg" ;
+        jugador1.src= "img/050-tiger-1.svg" ;
     }
 
-    if(nickname == "ardilla"){
+    if(avatar == "ardilla"){
         avatarJugador1.src= "img/003-chipmunk.svg" ;
+        jugador1.src= "img/003-chipmunk.svg" ;
     }
 }
 

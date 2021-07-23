8587d38aaf3c137cb154bced413f7539745222be
JUAN.TORRESVEGA <juan.torresvega@ucr.ac.cr>
2021-07-22 21:18:33 -0600

    session, avatar se elige

--- src/js/client/sessionHomePage.js
+++ src/js/client/sessionHomePage.js
@@ -2,15 +2,27 @@ function sessionMain() {
 
     //Identifica y guarda el valor del input para el nombre del jugador
     const nickNameElement = document.getElementById('jugadorId');
+    const avatarElement = document.getElementById('selectionofAvatars');
+
     console.assert(nickNameElement);
+    console.assert(avatarElement);
+
     const nickname = window.localStorage.getItem('jugadorId');
+    const avatar = window.localStorage.getItem('selectionofAvatars');
 
     if(nickname){
         nickNameElement.value= nickname;
     }
-    
+    if(avatar){
+        avatarElement.value= avatar;
+    }
+
     nickNameElement.addEventListener('input', () =>{
         window.localStorage.setItem('jugadorId',nickNameElement.value);
+    })
+
+    avatarElement.addEventListener('change', () =>{
+        window.localStorage.setItem('selectionofAvatars',avatarElement.value);
     })
 
 }
